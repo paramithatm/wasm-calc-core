526e60a71687bf24b582c2dfae9b598060f1177c
Paramitha <paramithatm@gmail.com>
2024-04-14 23:39:57 +0900

    update package.swift, remove executable

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "WasmCalcCore",
+    products: [
+            .library(name: "WasmCalcCore", targets: ["WasmCalcCore"]),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "wasm-calc-core"
+        .target(
+            name: "WasmCalcCore",
+            dependencies: []
         ),
     ]
 )
