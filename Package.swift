ef2134d79005ec21d3088481ca08d091e8f8fcef
Paramitha <paramithatm@gmail.com>
2024-04-14 23:11:14 +0900

    restore

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "wasm-calc-core",
+    name: "WasmCalcCore",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
